5872619684a5f226e3ff9833b1885be76b9fdfed
Milan <milan@Milans-MacBook-Air.local>
2023-05-30 10:42:30 +0530

    second commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js';
 
 //@desc Auth user/set token
 //route POST/api/users
 //@access Public
-const authUser = asyncHandler((req,res)=>{
-    res.status(200).json({message:'Auth User'})
+const authUser = asyncHandler(async(req,res)=>{
+    const {email,password} = req.body;
+    
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))){
+        generateToken(res,user._id);
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 }); 
 
 //@desc Register a new user
@@ -29,6 +44,7 @@ const registerUser = asyncHandler(async (req,res)=>{
 
     //confiorm user is created
     if(user){
+        generateToken(res,user._id);
         res.status(201).json({
             _id:user._id,
             name:user.name,
