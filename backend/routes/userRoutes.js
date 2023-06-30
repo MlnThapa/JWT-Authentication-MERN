5a05761008fc161f923514dd75f7f2e086c00dfa
Milan <milan@Milans-MacBook-Air.local>
2023-06-30 13:53:47 +0530

    completed JWT

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,11 +5,12 @@ import { authUser,
          getUserProfile,
          updateUserProfile} from '../controllers/userController.js';
 const router = express.Router();
+import {protect} from '../middleware/authMiddleware.js'
 
 router.post('/',registerUser)
 router.post('/auth',authUser)
 router.post('/logout',logoutUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser';
 dotenv.config()
 const PORT = process.env.PORT || 4000
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser())
 
 app.use('/api/users',userRoutes)
 app.get('/',(req,res)=>{
